47dd560dba3323389b27b87030bab109f7f9769c
vedoalfarizi <vedo.alfarizi@kitabisa.com>
2024-12-07 15:51:36 +0700

    enhance signout

--- src/app/auth/signout/route.ts
+++ src/app/auth/signout/route.ts
@@ -3,18 +3,30 @@ import { revalidatePath } from 'next/cache'
 import { type NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest) {
-  const supabase = await createClient()
+  try {
+    const supabase = await createClient()
 
-  // Check if a user's logged in
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
+    // Check if a user is logged in
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
 
-  if (user) {
-    await supabase.auth.signOut()
+    // If a user is logged in, sign them out
+    if (user) {
+      await supabase.auth.signOut()
+      console.log('Successfully logout user email: '+ user.email)
+    }
+  } catch (error) {
+    console.error('Error during signout:', error)
+    return NextResponse.json(
+      { error: 'Something went wrong during signout. Please try again later.' },
+      { status: 500 }
+    )
   }
 
-  revalidatePath('/', 'layout')
+  // Revalidate the homepage path to ensure the session state is updated
+  revalidatePath('/')
+
   return NextResponse.redirect(new URL('/login', req.url), {
     status: 302,
   })
