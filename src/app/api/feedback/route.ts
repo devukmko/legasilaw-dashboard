d63b1d7d7eb48f6b82b19f4a63b7c2926fab1be1
Muhammad Ilham <muhammad.ilhamsa1@gmail.com>
2024-12-10 01:17:28 +0700

    feat: add origin

--- src/app/api/feedback/route.ts
+++ src/app/api/feedback/route.ts
@@ -1,5 +1,27 @@
 import { createClient } from "@/utils/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+
+// Change the dynamic behavior of a layout or page to fully static or fully dynamic. https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
+export const dynamic = 'auto'
+// Set the default revalidation time for a layout or page. https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
+export const revalidate = 0
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#fetchcache
+export const fetchCache = 'auto'
+
+const origin = process.env.ORIGIN as string;
+
+export async function OPTIONS(_request: NextRequest) {
+  return new NextResponse(null, {
+    status: 204,
+    headers: {
+      'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  })
+}
+
 
 export async function POST(req: Request) {
   const supabase = await createClient();
@@ -26,7 +48,7 @@ export async function POST(req: Request) {
   const { fullname, phoneNumber, email, message } = validate.data;
 
   // Insert feedback into the database
-  const { error } = await supabase.from("feedbacks").insert({
+  const { data, error } = await supabase.from("feedbacks").insert({
     full_name: fullname,
     phone_number: phoneNumber,
     email: email || null,
